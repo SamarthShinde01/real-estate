5ba7c2465c49f741a3aaf96da2c265a92071dd02
Samarth Shinde <samarthsshinde@gmail.com>
2025-02-05 12:25:30 +0530

    client update

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
-  withCredentials: true,
+	baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+	withCredentials: true,
 });
 
 export default apiRequest;
